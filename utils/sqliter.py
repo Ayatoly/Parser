0f89c88bf4e65567990f0b12e33680e22d93d563
MariaSolovieva <58192545+MariaSolovieva@users.noreply.github.com>
2021-07-31 17:44:12 +0200

    Update sqliter.py

--- utils/sqliter.py
+++ utils/sqliter.py
@@ -1,20 +1,22 @@
-import sqlite3
-from parser import parce
 from datetime import datetime
-from hendlers import sned_messange
+import sqlite3
+
+from parser import parce
+from hendlers import send_messange
+
 
 class SQLighter:
 
     def __init__(self):
-        '''Подключаемся к БД и вызываем курсор соединения'''
+        """Подключаемся к БД и вызываем курсор соединения"""
         self.connection = sqlite3.connect('db.db')
         self.cursor = self.connection.cursor()
 
-    def get_all_ids(self):
+    def get_all_ids(self) -> None:
         return self.cursor.execute('select * from "id_users"').fetchall()
 
-    def get_subscription(self, status=True):
-        '''Получаем всех активных подписчеков бота'''
+    def get_subscription(self, status: bool = True) -> tuple:
+        """Получаем всех активных подписчеков бота"""
         result = self.cursor.execute(
             'SELECT * FROM "id_users" WHERE "status" = ?',
             (status,),
@@ -22,15 +24,15 @@ class SQLighter:
         self.connection.commit()
         return result
 
-    def subcripter_exists(self, user_id):
-        '''Проверяем есть ли юзер в базе'''
+    def subcripter_exists(self, user_id: int) -> bool:
+        """Проверяем есть ли юзер в базе"""
         result = self.cursor.execute(
             'SELECT * FROM "id_users" WHERE "user_id" = ?',
             (user_id,),
         ).fetchall()
         return bool(len(result))
 
-    def add_subcripter(self, user_id, status=True):
+    def add_subcripter(self, user_id: int, status: bool = True) -> None:
         user_ids = self.cursor.execute('select * from "id_users"').fetchall()
         if user_id not in user_ids:
             try:
@@ -43,27 +45,23 @@ class SQLighter:
                  print('Он уже есть в базе')
 
 
-    def update_subcriptions(self,user_id,status):
-        '''Обновляем статус подписки'''
+    def update_subcriptions(self, user_id: int, status: bool) -> tuple:
+        """Обновляем статус подписки"""
         return self.cursor.execute('UPDATE "id_users" SET "status" = ? WHERE "user_id" = ?',(status,user_id))
 
-    def add_car(self):
+    def add_car(self) -> None:
+        """Добавление машины в базу"""
         for i in parce():
             try:
                  self.cursor.execute('INSERT INTO "Jeep_auto_ru"(links,price,city,date) VALUES (?,?,?,?)',
                                      (i[0],i[1],i[2],datetime.now()))
                  self.connection.commit()
-                 sned_messange(i[0])
-
+                 send_messange(i[0])
             except:
                 pass
 
 
-    def close(self):
-        '''Закрываем соединение с БД'''
+    def close(self) -> None:
+        """Закрываем соединение с БД"""
         self.connection.close()
-
-
-
-
 
